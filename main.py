ad3e20b985f7eb3a9966020372c516ff0a8996f2
psturum <pmo.sturm@hotmail.com>
2021-09-23 00:17:48 +0200

    dwad

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PIL import Image
 model_best = load_model('best_model_14class.hdf5', compile = False)
 img_plates = Image.open("pics/4_plate.png")
 
+
 def listOfTuples(l1, l2):
     return list(map(lambda x, y:(x,y), l1, l2))
 
--- visualization.py
+++ visualization.py
@@ -1,4 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
+from math import floor
+
+font_ = ImageFont.truetype("font/AmaticSC-Regular.ttf", 150)
 spoilage = Image.open("pics/food_spoilage1.png")
 
 def quater_image(img):
@@ -24,6 +27,18 @@ def stack_images(tuple_list):
     spoilage.paste(img2, (280, 525))
     spoilage.paste(img1, (280, 825))
     spoilage.paste(img0, (280, 1120))
+
+
+    #Skal automatisere dage/dag
+    drawing = ImageDraw.Draw(spoilage)
+    drawing.text((25, 250), str(floor(tuple_list[3][0])) + " dage", fill=(255, 255, 255), font = font_)
+    drawing.text((25, 540), str(floor(tuple_list[2][0])) + " dage", fill=(255, 255, 255), font = font_)
+    drawing.text((25, 840), str(floor(tuple_list[1][0])) + " dage", fill=(255, 255, 255), font = font_)
+    drawing.text((25, 1140), str(floor(tuple_list[0][0])) + " dag", fill=(255, 255, 255), font = font_)
+
+
+    
+
     spoilage.show()
 
 
